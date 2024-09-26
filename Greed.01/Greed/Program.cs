1109db56fae883b11cee1ffe03fc6bd53db84a8f
Markéta Horáková <marketa.horakova@thermofisher.com>
2024-09-26 21:51:53 +0200

    Create Greed.01

---
+++ Greed.01/Greed/Program.cs
@@ -0,0 +1,11 @@
+﻿using Greed;
+
+RollDice greed = new RollDice();
+
+for (int i = 0; i < 200; i++)
+{
+    Console.Write(greed.ToRoll());
+    Console.Write(" - ");
+    Console.WriteLine(greed.ToCalculate());
+}
+
