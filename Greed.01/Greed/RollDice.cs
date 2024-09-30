e08e616c7b785abb04adb056710e8d2e9c579cb1
Pavol Ulbrich <palcifer@users.noreply.github.com>
2024-09-30 22:03:06 +0200

    Update RollDice.cs
    Komentar k nepovinnemu ukolu, písal som to bez prekladača do guthubu tak to asi nepôjde preložiť :-/ stačí si ale prečítať

--- Greed.01/Greed/RollDice.cs
+++ Greed.01/Greed/RollDice.cs
@@ -8,79 +8,49 @@ namespace Greed
 {
     public class RollDice
     {
+        //Celkovo super práca, vidno, že si sa nad tým zamyslela, systematicky si si to rozdelila na menšie problémy a potom postupne vyriešila. Mňa to teda napadlo úplne inak :D skvelo zvládnuté zadanie
+        //Tie komentáre nižšie sú len také rady k lepším praktikám, ktoré sa ti môžu zíjsť do budúcnosti
+        //Jedna veľká vec, ktorá je asi nad rámec tohto kurzu, je škálovateľnosť tohto kódu. Keby ti niekto povedal, že teraz prerob túto hru na kocku s 20 stranami, tak by to bolo veľmi veľmi bolestivé to prepisovať. Ale postupne sa naučíš myslieť viac genericky, uvidíš :-) 
         Random random = new Random();
         int[] arrayNumbers = new int[6];
-        int oneCounter = 0;
-        int twoCounter = 0;
-        int threeCounter = 0;
-        int fourCounter = 0;
-        int fiveCounter = 0;
-        int sixCounter = 0;
+        int[] counter = new int[6]; //máš 6 premenných pod sebou, každá ďalšia o jednu väčšia ako predchádzajúca. Prečo nevyužiť pole? Pozri, ako ti to potom zjednoduší metódu ToRoll() alebo TreePairs() :-)
 
         public string ToRoll()
         {
-            oneCounter = 0;
-            twoCounter = 0;
-            threeCounter = 0;
-            fourCounter = 0;
-            fiveCounter = 0;
-            sixCounter = 0;
+            //Dvakrát priradzuješ do jednej premennej hneď po sebe. Obecne to nie je chyba, ale je to neefektívne. Toto sú integeri, ale v prípade objektov to môže byť zbytočne náročné.
             for (int i = 0; i < arrayNumbers.Length; i++)
             {
                 arrayNumbers[i] = random.Next(1, 7);
             }
 
-            System.Text.StringBuilder builder = new System.Text.StringBuilder();
+            var builder = ""; //Super uvažovanie, do budúcna sa ti takýto prístup bude určite hodiť, že budeš mať objekty, ktoré ti pripravujú dáta na spracovanie. Akurát že pre tento jednoduchý príklad bohate stačilo var builder = ""; :-) 
             foreach (int number in arrayNumbers)
             {
                 builder.Append(number);
-                switch (number)
-                {
-                    case 1:
-                        oneCounter++;
-                        break;
-                    case 2:
-                        twoCounter++;
-                        break;
-                    case 3:
-                        threeCounter++;
-                        break;
-                    case 4:
-                        fourCounter++;
-                        break;
-                    case 5:
-                        fiveCounter++;
-                        break;
-                    case 6:
-                        sixCounter++;
-                        break;
-                }
+                counter[number]++; //
             }
 
             return builder.ToString();
 
         }
 
-        public string ToCalculate()
+        public string ToCalculate() //inak toto by som spravil úplne rovnako, najprv otestoval tie dve špeciálne prípady, a potom sa vrhol na jednotlivé čisla..
         {
             int resultNumber = 0;
 
-            if (TreePairs(oneCounter, twoCounter, threeCounter, fourCounter, fiveCounter, sixCounter))
+            if (TreePairs(counter))
             {
                 resultNumber = 800;
             }
-            else if (oneCounter == 1 && twoCounter == 1 && threeCounter == 1 && fourCounter == 1 && fiveCounter == 1 && sixCounter == 1)
+            else if (oneCounter == 1 && twoCounter == 1 && threeCounter == 1 && fourCounter == 1 && fiveCounter == 1 && sixCounter == 1) //toto by sa dalo implementovať podobne ako ThreePairs(), akurát sa tie county musia rovnať 1
             {
                 resultNumber = 1200;
             }
             else
             {
-                resultNumber += (Counter(oneCounter, 1000) + OneFiveCounter(oneCounter, 100));
-                resultNumber += Counter(twoCounter, 200);
-                resultNumber += Counter(threeCounter, 300);
-                resultNumber += Counter(fourCounter, 400);
-                resultNumber += (Counter(fiveCounter, 500) + OneFiveCounter(fiveCounter, 50));
-                resultNumber += Counter(sixCounter, 600);
+                resultNumber += Counter(oneCounter, 1000) + OneFiveCounter(oneCounter, 100) + Counter(twoCounter, 200) + Counter(threeCounter, 300) 
+                    + Counter(fourCounter, 400) + Counter(fiveCounter, 500) + OneFiveCounter(fiveCounter, 50) + Counter(sixCounter, 600); 
+                //toto kvôli čitateľnosti, šesť priradení do premennej pod sebou pôsobí divne. Akože tento zápis je robí doslova dopísmena to isté, ale menej znakov mi príde prehľadnejších.
 
             }
             return "Win points: " + resultNumber.ToString();
@@ -89,23 +59,10 @@ namespace Greed
 
         private int Counter(int rolledDiceNumber, int dicePointValue)
         {
-            int counterNumber = 0;
-            switch (rolledDiceNumber)
-            {
-                case 6:
-                    counterNumber += 8 * dicePointValue;
-                    break;
-                case 5:
-                    counterNumber += 4 * dicePointValue;
-                    break;
-                case 4:
-                    counterNumber += 2 * dicePointValue;
-                    break;
-                case 3:
-                    counterNumber += 1 * dicePointValue;
-                    break;
-            }
-            return counterNumber;
+            //toto už bude trošku zložitejšie. Keď si všimneš, tak čísla 1, 2, 4, 8 sú mocniny 2
+            //Takže namiesto switchu vieš napísať krásnu formulku
+            var multiplier = Math.Pow(2, rolledDiceNumber-3);
+            return dicePointValue * multiplier;
         }
 
         private int OneFiveCounter(int rolledDiceNumber, int dicePointValue)
@@ -123,32 +80,11 @@ namespace Greed
             return counterNumber;
         }
 
-        private bool TreePairs(int one, int two, int three, int four, int five, int six)
+        private bool TreePairs(int[] counter)
         {
             int pairNumber = 0;
-            if (one == 2)
-            {
-                pairNumber += 1;
-            }
-            if (two == 2)
-            {
-                pairNumber += 1;
-            }
-            if (three == 2)
-            {
-                pairNumber += 1;
-            }
-            if (four == 2)
-            {
-                pairNumber += 1;
-            }
-            if (five == 2)
-            {
-                pairNumber += 1;
-            }
-            if (six == 2)
-            {
-                pairNumber += 1;
+            foreach count in counter {
+                if (count == 2) pairNumber++;
             }
             return (pairNumber == 3);
         }
