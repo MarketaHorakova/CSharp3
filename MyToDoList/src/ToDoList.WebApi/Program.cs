947d37343b6148377bce2f28f72d238e08d8f739
Markéta Horáková <marketa.horakova@thermofisher.com>
2024-10-05 09:24:26 +0200

    Add aditional paths

--- MyToDoList/src/ToDoList.WebApi/Program.cs
+++ MyToDoList/src/ToDoList.WebApi/Program.cs
@@ -1,6 +1,10 @@
+using System.Security.Cryptography;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/nazdarSvete", () => "Ahoj světe!");
+app.MapGet("/pozdravKouce", () => "Ahoj Paľko!");
 
 app.Run();
